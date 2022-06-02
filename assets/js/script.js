d29c12b0002a97dc17fda8bb6128b79d63874754
-l <dbonineiv@gmail.com>
2022-06-01 21:41:22 -0700

    added localized date format. Search now populates current weather, altered css and html to suit.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,34 @@ var city = "Seattle";
 var state = "Washington;";
 var country = "US";
 var units = "";
+var tempunit = "";
+var windunit = "";
 var lat = "33.44";
 var lon = "-94.04";
-//Relevant to: Print
-const d = new Date();
-const date = d.toDateString();
 //Relevant to: searchbtn event listener
 const citysearch = document.querySelector("#searchbox");
 const searchbtn = document.querySelector("#searchbtn");
+//Relevant to: Print
+const options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+var date = new Date().toLocaleDateString("en-UK", options);
+const currentcity = document.querySelector("#currentcity");
+const currentdate = document.querySelector("#currentdate");
+const currenticon = document.getElementById("currenticon");
+const currenttemp = document.querySelector("#currenttemp");
+const currentwind = document.querySelector("#currentwind");
+const currenthumidity = document.querySelector("#currenthumidity");
+const currentuvindex = document.querySelector("#currentuvindex");
+
+//Listen for user search
+searchbtn.addEventListener("click", function () {
+  city = citysearch.value;
+  geoCode();
+});
 
 //Call geocoding api for accurate search
 function geoCode(data) {
@@ -30,6 +50,40 @@ function geoCode(data) {
       stupidUnits(data);
     })
     .catch((err) => console.error(err));
+}
+
+function parseData(data) {
+  console.log("parseData", data);
+  country = data[0].country;
+  city = data[0].name;
+  lat = data[0].lat;
+  lon = data[0].lon;
+  citysearch.value = city + ", " + country;
+  if (country === "US") {
+    var statename = data[0].state;
+    stateNameToAbbreviation(statename);
+    console.log("state code: " + state);
+    citysearch.value = data[0].name + " " + state + ", " + data[0].country;
+  }
+}
+
+function stupidUnits(data) {
+  console.log("stupidUnits", country);
+  let array = ["US", "BS", "KY", "LR", "PW", "FM", "MH"];
+  if (array.includes(country)) {
+    units = "imperial";
+    tempunit = "°F";
+    windunit = "mi/h";
+    console.log("Units are stupid");
+    date = new Date().toLocaleDateString("en-US", options);
+  } else {
+    console.log("Your units are correct");
+    units = "metric";
+    tempunit = "°C";
+    windunit = "m/h";
+    date = new Date().toLocaleDateString("en-UK", options);
+  }
+  getWeather();
 }
 
 function getWeather(data) {
@@ -54,56 +108,43 @@ function getWeather(data) {
     .catch((err) => console.error(err));
 }
 
-//Listen for user search
-searchbtn.addEventListener("click", function () {
-  city = citysearch.value;
-  geoCode();
-});
-
 //Populate current weather
 function Print(data) {
   console.log("printing...");
   //city name not contained in data
+  currentcity.innerHTML = city;
   console.log(date);
-  console.log(data.current.temp);
+  currentdate.innerHTML = date;
   console.log(data.current.weather.icon);
+  let imgcode = data.current.weather[0].icon;
+  let imgalt = data.current.weather[0].description;
+  currenticon.src = "http://openweathermap.org/img/wn/" + imgcode + "@2x.png";
+  currenticon.title = imgalt;
+  console.log(data.current.temp);
+  currenttemp.innerHTML = data.current.temp + tempunit;
   console.log(data.current.wind_speed);
+  currentwind.innerHTML = data.current.wind_speed + windunit;
   console.log(data.current.humidity);
+  currenthumidity.innerHTML = data.current.humidity + "%";
   console.log(data.current.uvi);
+  if (11 <= data.current.uvi) {
+    currentuvindex.style.background = "#7b439c";
+  } else if (8 <= data.current.uvi) {
+    currentuvindex.style.background = "#ee154a";
+  } else if (6 <= data.current.uvi) {
+    currentuvindex.style.background = "#f66b34";
+  } else if (3 <= data.current.uvi) {
+    currentuvindex.style.background = "#fcbd22";
+  } else if (0 <= data.current.uvi) {
+    currentuvindex.style.background = "#67be4d";
+  }
+  currentuvindex.innerHTML = data.current.uvi;
   console.log(data.daily);
-}
-
-function parseData(data) {
-  console.log("parseData", data);
-  country = data[0].country;
-  city = data[0].name;
-  lat = data[0].lat;
-  lon = data[0].lon;
-  citysearch.value = city + ", " + country;
-  if (country === "US") {
-    var statename = data[0].state;
-    stateNameToAbbreviation(statename);
-    console.log("state code: " + state);
-    citysearch.value = data[0].name + " " + state + ", " + data[0].country;
-  }
-}
-
-function stupidUnits(data) {
-  console.log("stupidUnits", country);
-  let array = ["US", "BS", "KY", "LR", "PW", "FM", "MH"];
-  if (array.includes(country)) {
-    units = "imperial";
-    console.log("Units are stupid");
-  } else {
-    console.log("Your units are correct");
-    units = "metric";
-  }
-  getWeather();
 }
 
 //Autorun for testing
 // getWeather();
-geoCode();
+// geoCode();
 // stateNameToAbbreviation(state);
 
 //Convert State Name to State Code
