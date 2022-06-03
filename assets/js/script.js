14d43100ef9a9ec3c411432cb6c55193031c254e
-l <dbonineiv@gmail.com>
2022-06-02 21:34:28 -0700

    search now populates 5-day forecast. Added previous searches via local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-//fetch from weather API
 //Key: 595716eb9ad5fdcbf4ae8889267dc2d6
 //Relevant to: geoCode, getWeather
 const apikey = "595716eb9ad5fdcbf4ae8889267dc2d6";
@@ -14,13 +13,14 @@ var lon = "-94.04";
 const citysearch = document.querySelector("#searchbox");
 const searchbtn = document.querySelector("#searchbtn");
 //Relevant to: Print
+var dateformat = "en-UK";
 const options = {
   weekday: "long",
   year: "numeric",
   month: "long",
   day: "numeric",
 };
-var date = new Date().toLocaleDateString("en-UK", options);
+var date = new Date();
 const currentcity = document.querySelector("#currentcity");
 const currentdate = document.querySelector("#currentdate");
 const currenticon = document.getElementById("currenticon");
@@ -28,6 +28,23 @@ const currenttemp = document.querySelector("#currenttemp");
 const currentwind = document.querySelector("#currentwind");
 const currenthumidity = document.querySelector("#currenthumidity");
 const currentuvindex = document.querySelector("#currentuvindex");
+const futureweather = document.querySelector("#futureweather");
+//Relevant to: addSavedCities
+const savedsearches = document.querySelector("#savedsearches");
+var citystring = localStorage.getItem("citykey", "packcity");
+var savedcities = JSON.parse(citystring);
+if (savedcities == null) {
+  savedcities = [];
+} else {
+  printSavedCities();
+}
+
+//Call functions for testing
+// getWeather();
+// geoCode();
+// stateNameToAbbreviation(state);
+// hidePlaceholders();
+// localStorage.clear();
 
 //Listen for user search
 searchbtn.addEventListener("click", function () {
@@ -45,50 +62,48 @@ function geoCode(data) {
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log("Geocode", data);
       parseData(data);
       stupidUnits(data);
     })
     .catch((err) => console.error(err));
 }
 
+//store relevant data in variables
 function parseData(data) {
-  console.log("parseData", data);
   country = data[0].country;
   city = data[0].name;
   lat = data[0].lat;
   lon = data[0].lon;
   citysearch.value = city + ", " + country;
+  //correct date format
   if (country === "US") {
+    dateformat = "en-US";
     var statename = data[0].state;
     stateNameToAbbreviation(statename);
-    console.log("state code: " + state);
     citysearch.value = data[0].name + " " + state + ", " + data[0].country;
   }
+  saveCity();
 }
 
+//Changes units to imperial or metric based on country searched
 function stupidUnits(data) {
-  console.log("stupidUnits", country);
   let array = ["US", "BS", "KY", "LR", "PW", "FM", "MH"];
   if (array.includes(country)) {
     units = "imperial";
     tempunit = "°F";
     windunit = "mi/h";
-    console.log("Units are stupid");
-    date = new Date().toLocaleDateString("en-US", options);
+    console.log("Imperial units are stupid");
   } else {
-    console.log("Your units are correct");
+    console.log("Metric is the superior measurement system.");
     units = "metric";
     tempunit = "°C";
     windunit = "m/h";
-    date = new Date().toLocaleDateString("en-UK", options);
   }
   getWeather();
 }
 
+//get weather data from
 function getWeather(data) {
-  //   geoCode();
-  console.log("units returned: ", units);
   fetch(
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
       lat +
@@ -102,31 +117,26 @@ function getWeather(data) {
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log("getWeather", data);
       Print(data);
     })
     .catch((err) => console.error(err));
 }
 
-//Populate current weather
+//Populate weather data onto page
 function Print(data) {
-  console.log("printing...");
-  //city name not contained in data
+  console.log("printing data...");
+  hidePlaceholders();
   currentcity.innerHTML = city;
-  console.log(date);
-  currentdate.innerHTML = date;
-  console.log(data.current.weather.icon);
+  currentdate.innerHTML = date.toLocaleDateString(dateformat, options);
   let imgcode = data.current.weather[0].icon;
   let imgalt = data.current.weather[0].description;
   currenticon.src = "http://openweathermap.org/img/wn/" + imgcode + "@2x.png";
   currenticon.title = imgalt;
-  console.log(data.current.temp);
   currenttemp.innerHTML = data.current.temp + tempunit;
-  console.log(data.current.wind_speed);
   currentwind.innerHTML = data.current.wind_speed + windunit;
-  console.log(data.current.humidity);
   currenthumidity.innerHTML = data.current.humidity + "%";
-  console.log(data.current.uvi);
+
+  //Color code UV index
   if (11 <= data.current.uvi) {
     currentuvindex.style.background = "#7b439c";
   } else if (8 <= data.current.uvi) {
@@ -139,13 +149,87 @@ function Print(data) {
     currentuvindex.style.background = "#67be4d";
   }
   currentuvindex.innerHTML = data.current.uvi;
-  console.log(data.daily);
+
+  //Print daily forecast
+  futureweather.innerHTML = ""; //clear display
+  let forecast = data.daily;
+  for (let i = 0; i < 5; i++) {
+    const daily = forecast[i];
+
+    //create and append display card
+    let day = document.createElement("figure");
+    day.classList.add("futureweathercard");
+    futureweather.appendChild(day);
+
+    //create and append weather icon
+    let fimgcode = daily.weather[0].icon;
+    let fimagealt = daily.weather[0].description;
+    let ficon = document.createElement("img");
+    ficon.classList.add("icon");
+    ficon.src = "http://openweathermap.org/img/wn/" + fimgcode + "@2x.png";
+    ficon.title = fimagealt;
+    day.appendChild(ficon);
+
+    //create and append appropriate date
+    let fdate = document.createElement("p");
+    var newdate = new Date();
+    //Add [i] days to current date
+    Date.prototype.addDays = function (days) {
+      let day = new Date(this.valueOf());
+      day.setDate(day.getDate() + days);
+      return day;
+    };
+    fdate.innerHTML =
+      "Date: " + newdate.addDays(i).toLocaleDateString(dateformat, options);
+    day.appendChild(fdate);
+
+    //create and append temperature, wind speed, and humidity
+    let ftemp = document.createElement("p");
+    ftemp.innerHTML = "Temp: " + daily.temp.day + tempunit;
+    day.appendChild(ftemp);
+    let fwind = document.createElement("p");
+    fwind.innerHTML = "Wind: " + daily.wind_speed + windunit;
+    day.appendChild(fwind);
+    let fhumidity = document.createElement("p");
+    fhumidity.innerHTML = "Humidity: " + daily.humidity + "%";
+    day.appendChild(fhumidity);
+  }
+  printSavedCities();
 }
 
-//Autorun for testing
-// getWeather();
-// geoCode();
-// stateNameToAbbreviation(state);
+//Hide placeholders
+function hidePlaceholders() {
+  let placeholders = document.querySelectorAll(".placeholder");
+  for (let i = 0; i < placeholders.length; i++) {
+    const placeholder = placeholders[i];
+    placeholder.classList.add("hidden");
+  }
+}
+
+//Add city to localstorage
+function saveCity() {
+  if (!savedcities.includes(city)) {
+    savedcities.push(city);
+    let packcity = JSON.stringify(savedcities);
+    localStorage.setItem("citykey", packcity);
+  }
+}
+
+//Display previous searches on page
+function printSavedCities() {
+  savedsearches.innerHTML = ""; //clear display
+  for (let i = 0; i < savedcities.length; i++) {
+    const cityname = savedcities[i];
+    let citycard = document.createElement("h3");
+    citycard.classList.add("savedcity");
+    citycard.innerHTML = cityname;
+    savedsearches.appendChild(citycard);
+    citycard.addEventListener("click", function () {
+      city = cityname;
+      geoCode();
+    });
+  }
+}
 
 //Convert State Name to State Code
 function stateNameToAbbreviation(statename) {
@@ -212,6 +296,5 @@ function stateNameToAbbreviation(statename) {
   let a = statename.trim().replace(";", "").toLowerCase();
   if (states[a] !== null) {
     state = states[a];
-    console.log("state changed to " + state);
   }
 }
